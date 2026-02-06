f0ede626a6de7e073a5c0962e8323842518e85b9
vonvit <sandrineviturn@gmail.com>
2026-02-06 15:46:51 -0300

    adicionando e removendo elementos da lista

--- praticandoListas/src/main/java/application/Program.java
+++ praticandoListas/src/main/java/application/Program.java
@@ -1,4 +1,28 @@
 package application;
 
+import java.util.ArrayList;
+import java.util.List;
+
 public class Program {
+    void main(){
+        //lista nao aceita tipos primitivos.ex(int)
+
+        List<String> list = new ArrayList<>();
+
+        //adicionando elementos na lista
+        list.add("lilica");
+        list.add("vonvit");
+        list.add("clara");
+        list.add(2, "nãnã");//adicionando em uma posição especifica
+
+        System.out.println(list.size());
+
+        //removendo elementos da lista
+        list.remove("lilica");
+        list.remove(1);
+
+        for(String x : list){
+            System.out.println(x);
+        }
+    }
 }
