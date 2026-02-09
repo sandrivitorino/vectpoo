aa2570bd6ae40565416b148b7ef5c8d2af30955b
vonvit <sandrineviturn@gmail.com>
2026-02-08 21:58:24 -0300

    Encontrando a quantidade de negativos.

--- excTestMatriz/src/main/java/application/Program.java
+++ excTestMatriz/src/main/java/application/Program.java
@@ -25,6 +25,18 @@ public class Program {
             System.out.print(mat[i][i] + " ");
         }
 
+        System.out.println();
+
+        int count = 0;
+        for(int i = 0; i<n; i++){
+            for(int j=0; j<n; j++){
+                if(mat[i][j] < 0){
+                    count++;
+                }
+            }
+        Sytem.out.println("Negative numbers = " + count);
+        }
+
         sc.close();
     }
 }
